d95ca6824bfcb5407f1bdeec7b9f8dc4f9fc921a
Michael Chan <mikecchan0@gmail.com>
2018-04-11 11:01:19 -0400

    first commit

---
+++ spec/AddressBookSpec.js
@@ -0,0 +1,12 @@
+// add a contact us a useful feature
+
+describe('Address Book', function()) {
+  it('should be able to add a contact', function()){
+    var addressBook = new AddressBook(),
+    thisContact = new Contact();
+
+    addressBook.addContact(thisContact);
+
+    expect(addressBook.getContact(0)).toBe(thisContact);
+  });
+}
