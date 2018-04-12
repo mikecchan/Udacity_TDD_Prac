f766b616cdff36842735fd48250a873fc634e8d5
Michael Chan <mikecchan0@gmail.com>
2018-04-12 12:56:52 -0400

    day two

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -1,5 +1,5 @@
 // add a contact us a useful feature
-
+console.log("AddressBookSpec.js");
 describe('Address Book', function() {
   it('should be able to add a contact', function(){
     var addressBook = new AddressBook(),
--- src/AddressBook.js
+++ src/AddressBook.js
@@ -1,4 +1,5 @@
 function AddressBook() {
+  console.log("AddressBook()");
   this.contacts = [];
 }
 
